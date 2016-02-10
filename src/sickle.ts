angular/tsickle

commit 16a0f692312006e68a515d000389e80a08f4a7e1

emit full jsdoc for function types
Rather than emitting a type per parameter, emit a jsdoc
before the function declaration.  This allows us to mark optional
arguments properly with the = suffix.  Note the changed "optional.ts"
test; it now verifies that Closure accepts a missing argument when
that argument was missing before.

Fixes #43.

src/sickle.ts
@@ -73,7 +73,7 @@ class Annotator {
     this.indent++;
     switch (node.kind) {
       case ts.SyntaxKind.VariableDeclaration:
-        this.maybeVisitType((<ts.VariableDeclaration>node).type);
+        this.maybeEmitJSDocType((<ts.VariableDeclaration>node).type);
         this.writeNode(node);
         break;
       case ts.SyntaxKind.ClassDeclaration: {
@@ -124,25 +124,33 @@ class Annotator {
       case ts.SyntaxKind.FunctionDeclaration:
       case ts.SyntaxKind.ArrowFunction:
         let fnDecl = <ts.FunctionLikeDeclaration>node;
-        this.maybeVisitType(fnDecl.type, '@return');
         let writeOffset = fnDecl.getFullStart();
+        writeOffset = this.writeRange(writeOffset, fnDecl.getStart());
+        // The first \n makes the output sometimes uglier than necessary,
+        // but it's needed to work around
+        // https://github.com/Microsoft/TypeScript/issues/6982
+        this.emit('\n/**\n');
         // Parameters.
         if (fnDecl.parameters.length) {
           for (let param of fnDecl.parameters) {
-            this.writeTextFromOffset(writeOffset, param);
-            writeOffset = param.getEnd();
-            let optional = param.initializer != null || param.questionToken != null;
-            this.maybeVisitType(param.type, null, optional);
-            this.visit(param);
+            if (param.type) {
+              let optional = param.initializer != null || param.questionToken != null;
+              this.emit(' * @param {');
+              this.emitType(param.type, optional);
+              this.emit('} ');
+              this.writeNode(param.name);
+              this.emit('\n');
+            }
           }
         }
         // Return type.
         if (fnDecl.type) {
-          this.writeTextFromOffset(writeOffset, fnDecl.type);
-          this.visit(fnDecl.type);
-          writeOffset = fnDecl.type.getEnd();
+          this.emit(' * @return {');
+          this.emitType(fnDecl.type);
+          this.emit('}\n');
         }
-        // Body.
+        this.emit(' */\n');
+
         this.writeTextFromOffset(writeOffset, fnDecl.body);
         this.visit(fnDecl.body);
         break;
@@ -203,7 +211,7 @@ class Annotator {
     if (existingAnnotation) {
       existingAnnotation += '\n';
     }
-    this.maybeVisitType(p.type, existingAnnotation + '@type');
+    this.maybeEmitJSDocType(p.type, existingAnnotation + '@type');
     this.emit('\nthis.');
     this.emit(p.name.getText());
     this.emit(';');
@@ -233,7 +241,7 @@ class Annotator {
     return '';
   }
 
-  private maybeVisitType(type: ts.TypeNode, jsDocTag?: string, optional?: boolean) {
+  private maybeEmitJSDocType(type: ts.TypeNode, jsDocTag?: string) {
     if (!type && !this.options.untyped) return;
     this.emit(' /**');
     if (jsDocTag) {
@@ -241,6 +249,14 @@ class Annotator {
       this.emit(jsDocTag);
       this.emit(' {');
     }
+    this.emitType(type);
+    if (jsDocTag) {
+      this.emit('}');
+    }
+    this.emit(' */');
+  }
+
+  private emitType(type: ts.TypeNode, optional?: boolean) {
     if (this.options.untyped) {
       this.emit(' ?');
     } else {
@@ -249,10 +265,6 @@ class Annotator {
     if (optional) {
       this.emit('=');
     }
-    if (jsDocTag) {
-      this.emit('}');
-    }
-    this.emit(' */');
   }
 
   private visitTypeAlias(node: ts.TypeAliasDeclaration) {

test_files/es6/basic.untyped.js
@@ -1,6 +1,10 @@
-/** @return { ?} */ // This test is just a random collection of typed code, to
+// This test is just a random collection of typed code, to
 // ensure the output is all with {?} annotations.
-function func(/** ? */ arg1) {
+/**
+ * @param { ?} arg1
+ * @return { ?}
+ */
+function func(arg1) {
     return [3];
 }
 class Foo {

test_files/es6/file_comment.js
@@ -1,4 +1,7 @@
-/** @return { string} */ // This test verifies that initial comments don't confuse offsets.
+// This test verifies that initial comments don't confuse offsets.
+/**
+ * @return { string}
+ */
 function foo() {
     return 'foo';
 }

test_files/es6/functions.js
@@ -1,4 +1,12 @@
-/** @return { string} */ function fn1(/** number */ a) {
+/**
+ * @param { number} a
+ * @return { string}
+ */
+function fn1(a) {
     return "a";
 }
-function fn2(/** number */ a, /** number */ b) { }
+/**
+ * @param { number} a
+ * @param { number}  b
+ */
+function fn2(a, b) { }

test_files/sickle/arrow_fn.ts
@@ -1,1 +1,6 @@
-var fn3 = /** @return { number} */ ( /** number */a: number): number => 12;
+var fn3 = 
+/**
+ * @param { number} a
+ * @return { number}
+ */
+(a: number): number => 12;

test_files/sickle/basic.untyped.ts
@@ -1,6 +1,11 @@
- /** @return { ?} */// This test is just a random collection of typed code, to
+// This test is just a random collection of typed code, to
 // ensure the output is all with {?} annotations.
-function func( /** ? */arg1: string): number[] {
+
+/**
+ * @param { ?} arg1
+ * @return { ?}
+ */
+function func(arg1: string): number[] {
   return [3];
 }
 

test_files/sickle/file_comment.ts
@@ -1,4 +1,8 @@
- /** @return { string} */// This test verifies that initial comments don't confuse offsets.
+// This test verifies that initial comments don't confuse offsets.
+
+/**
+ * @return { string}
+ */
 function foo(): string {
   return 'foo';
 }

test_files/sickle/functions.ts
@@ -1,4 +1,14 @@
- /** @return { string} */function fn1( /** number */a: number): string {
+
+/**
+ * @param { number} a
+ * @return { string}
+ */
+function fn1(a: number): string {
   return "a";
 }
-function fn2( /** number */a: number, /** number */ b: number) {}
+
+/**
+ * @param { number} a
+ * @param { number}  b
+ */
+function fn2(a: number, b: number) {}

commit b4c9b8e32c2922626c83a69dbfbef5718272aeeb

preserve closure annotations when emitting functions
The change in 16a0f692312006e68a515d000389e80a08f4a7e1 caused
us to emit a JSDoc comment before functions, which means we
would drop any already existing JSDoc comment.

Fix this by merging existing JSDocs with our new comment.
Add a test in functions.ts to cover this.  (The other changed
tests are just due to changing comment emits.)

src/sickle.ts
@@ -124,12 +124,14 @@ class Annotator {
       case ts.SyntaxKind.FunctionDeclaration:
       case ts.SyntaxKind.ArrowFunction:
         let fnDecl = <ts.FunctionLikeDeclaration>node;
-        let writeOffset = fnDecl.getFullStart();
-        writeOffset = this.writeRange(writeOffset, fnDecl.getStart());
         // The first \n makes the output sometimes uglier than necessary,
         // but it's needed to work around
         // https://github.com/Microsoft/TypeScript/issues/6982
         this.emit('\n/**\n');
+        let existingAnnotation = this.existingClosureAnnotation(node);
+        if (existingAnnotation) {
+          this.emit(' * ' + existingAnnotation + '\n');
+        }
         // Parameters.
         if (fnDecl.parameters.length) {
           for (let param of fnDecl.parameters) {
@@ -151,7 +153,7 @@ class Annotator {
         }
         this.emit(' */\n');
 
-        this.writeTextFromOffset(writeOffset, fnDecl.body);
+        this.writeTextFromOffset(fnDecl.getStart(), fnDecl.body);
         this.visit(fnDecl.body);
         break;
       case ts.SyntaxKind.TypeAliasDeclaration:
@@ -207,7 +209,7 @@ class Annotator {
   }
 
   private visitProperty(p: ts.PropertyDeclaration | ts.ParameterDeclaration) {
-    let existingAnnotation = this.existingClosureAnnotation(p).trim();
+    let existingAnnotation = this.existingClosureAnnotation(p);
     if (existingAnnotation) {
       existingAnnotation += '\n';
     }
@@ -221,8 +223,8 @@ class Annotator {
   /**
    * Returns empty string if there is no existing annotation.
    */
-  private existingClosureAnnotation(p: ts.PropertyDeclaration | ts.ParameterDeclaration) {
-    let text = p.getFullText();
+  private existingClosureAnnotation(node: ts.Node) {
+    let text = node.getFullText();
     let comments = ts.getLeadingCommentRanges(text, 0);
 
     if (!comments || comments.length == 0) return '';
@@ -230,7 +232,7 @@ class Annotator {
     // JS compiler only considers the last comment significant.
     let {pos, end} = comments[comments.length - 1];
     let comment = text.substring(pos, end);
-    return Annotator.getJsDocAnnotation(comment);
+    return Annotator.getJsDocAnnotation(comment).trim();
   }
 
   // return empty string if comment is not JsDoc.

test_files/es6/basic.untyped.js
@@ -1,5 +1,3 @@
-// This test is just a random collection of typed code, to
-// ensure the output is all with {?} annotations.
 /**
  * @param { ?} arg1
  * @return { ?}

test_files/es6/file_comment.js
@@ -1,4 +1,3 @@
-// This test verifies that initial comments don't confuse offsets.
 /**
  * @return { string}
  */

test_files/es6/functions.js
@@ -2,11 +2,17 @@
  * @param { number} a
  * @return { string}
  */
-function fn1(a) {
+function FunctionsTest1(a) {
     return "a";
 }
 /**
  * @param { number} a
  * @param { number}  b
  */
-function fn2(a, b) { }
+function FunctionsTest2(a, b) { }
+/**
+ * @ngInject
+ * @param { number} a
+ * @param { number}  b
+ */
+function FunctionsTest3(a, b) { }

test_files/functions.ts
@@ -1,4 +1,6 @@
-function fn1(a: number): string {
+function FunctionsTest1(a: number): string {
   return "a";
 }
-function fn2(a: number, b: number) {}
+function FunctionsTest2(a: number, b: number) {}
+/** @ngInject */
+function FunctionsTest3(a: number, b: number) {}

test_files/sickle/arrow_fn.ts
@@ -1,4 +1,4 @@
-var fn3 = 
+var fn3 =
 /**
  * @param { number} a
  * @return { number}

test_files/sickle/basic.untyped.ts
@@ -1,5 +1,3 @@
-// This test is just a random collection of typed code, to
-// ensure the output is all with {?} annotations.
 
 /**
  * @param { ?} arg1

test_files/sickle/file_comment.ts
@@ -1,4 +1,3 @@
-// This test verifies that initial comments don't confuse offsets.
 
 /**
  * @return { string}

test_files/sickle/functions.ts
@@ -3,12 +3,17 @@
  * @param { number} a
  * @return { string}
  */
-function fn1(a: number): string {
+function FunctionsTest1(a: number): string {
   return "a";
 }
-
 /**
  * @param { number} a
  * @param { number}  b
  */
-function fn2(a: number, b: number) {}
+function FunctionsTest2(a: number, b: number) {}
+/**
+ * @ngInject
+ * @param { number} a
+ * @param { number}  b
+ */
+function FunctionsTest3(a: number, b: number) {}