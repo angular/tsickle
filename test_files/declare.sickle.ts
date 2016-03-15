86e5e7ccb8110ab8dde76db713679caaca6fec39
Evan Martin <evanm@google.com>
2016-03-14 18:21:00 -0700

    blacklist some variable names for closure externs
    Don't create externs for variables that exist in Closure externs
    already, such as "module" and "global".

--- test_files/declare.sickle.ts
+++ test_files/declare.sickle.ts
@@ -19,3 +19,7 @@ declare var someGlobal: number;
 declare interface DeclareTestInterface {
   foo: string;
 }
+
+// Should be omitted from externs -- conflicts with Closure.
+declare var global: any;
+declare interface exports {}
