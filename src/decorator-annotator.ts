72f4d1528cc5b78ffc02cd5606dabb3ea1ad0da6
Evan Martin <evanm@google.com>
2016-04-26 13:43:11 -0700

    add a module for converting decorators to annotators
    Summary:
    This will be used by Angular2.  See the test case for examples
    of the transformation.
    
    Reviewers: alexeagle, mprobst, tbosch
    
    Reviewed By: alexeagle, mprobst
    
    Subscribers: tbosch, mprobst, IgorMinar, typescript-eng
    
    Differential Revision: https://reviews.angular.io/D84

---
+++ src/decorator-annotator.ts
@@ -0,0 +1,230 @@
+import * as sickle from './sickle';
+import * as ts from 'typescript';
+
+// ClassRewriter rewrites a single "class Foo {...}" declaration.
+// It's its own object because we collect decorators on the class and the ctor
+// separately for each class we encounter.
+class ClassRewriter extends sickle.Rewriter {
+  /** Decorators on the class itself. */
+  decorators: ts.Decorator[];
+  /** The constructor parameter list and decorators on each param. */
+  ctorParameters: [string, ts.Decorator[]][];
+  /** Per-method decorators. */
+  propDecorators: {[key: string]: ts.Decorator[]};
+
+  /**
+   * process is the main entry point, rewriting a single class node.
+   */
+  process(node: ts.ClassDeclaration): {output: string, diagnostics: ts.Diagnostic[]} {
+    if (node.decorators) {
+      this.decorators = node.decorators.slice();
+    }
+    let pos = node.getFullStart();
+    for (let child of node.getChildren()) {
+      switch (child.kind) {
+        case ts.SyntaxKind.CloseBraceToken:
+          // Before writing the close brace, dump the metadata.
+          this.writeRange(pos, child.getStart());
+          this.emitMetadata();
+          this.writeRange(child.getStart(), child.getEnd());
+          break;
+        default:
+          this.writeRange(pos, child.getFullStart());
+          this.visit(child);
+      }
+      pos = child.getEnd();
+    }
+    return this.getOutput();
+  }
+
+  /**
+   * gatherConstructor grabs the parameter list and decorators off the class
+   * constructor, and emits nothing.
+   */
+  private gatherConstructor(ctor: ts.ConstructorDeclaration) {
+    let ctorParameters: [string, ts.Decorator[]][] = [];
+    let hasDecoratedParam = false;
+    for (let param of ctor.parameters) {
+      let paramCtor: string;
+      let decorators: ts.Decorator[];
+      if (param.decorators) {
+        decorators = param.decorators.slice();
+      }
+      if (param.type) {
+        switch (param.type.kind) {
+          case ts.SyntaxKind.TypeReference:
+            let typeRef = param.type as ts.TypeReferenceNode;
+            // Type reference can be a bare name or a qualified name (foo.bar).
+            // There are also type arguments to worry about.  We are making the
+            // big assumption that a type reference is the same name as a ctor
+            // for that type, and it's simplest to just use the source text.
+            paramCtor = typeRef.getText();
+            break;
+          default:
+            // Some other type of type; just ignore it.
+        }
+      }
+      if (paramCtor) {
+        if (decorators) hasDecoratedParam = true;
+        ctorParameters.push([paramCtor, decorators]);
+      } else {
+        ctorParameters.push(null);
+      }
+    }
+
+    // Use the ctor parameter metadata only if the class or the ctor was decorated.
+    if (this.decorators || hasDecoratedParam) {
+      this.ctorParameters = ctorParameters;
+    }
+  }
+
+  /**
+   * gatherMethod grabs the decorators off a class method and emits nothing.
+   */
+  private gatherMethod(method: ts.MethodDeclaration) {
+    if (!method.decorators) return;
+    if (method.name.kind !== ts.SyntaxKind.Identifier) {
+      // Method has a weird name, e.g.
+      //   [Symbol.foo]() {...}
+      this.error(method, `cannot process decorators on ${ts.SyntaxKind[method.name.kind]}`);
+      return;
+    }
+
+    let name = (method.name as ts.Identifier).text;
+    let decorators: ts.Decorator[] = method.decorators.slice();
+    if (!this.propDecorators) this.propDecorators = {};
+    this.propDecorators[name] = decorators;
+  }
+
+  /**
+   * maybeProcess is called by the traversal of the AST.
+   * @return True if the node was handled, false to have the node emitted as normal.
+   */
+  protected maybeProcess(node: ts.Node): boolean {
+    switch (node.kind) {
+      case ts.SyntaxKind.ClassDeclaration:
+        // Encountered a new class while processing this class; use a new separate
+        // rewriter to gather+emit its metadata.
+        let {output, diagnostics} =
+            new ClassRewriter(this.file).process(node as ts.ClassDeclaration);
+        this.diagnostics.push(...diagnostics);
+        this.emit(output);
+        return true;
+      case ts.SyntaxKind.Constructor:
+        this.gatherConstructor(node as ts.ConstructorDeclaration);
+        return false;  // Proceed with ordinary emit of the ctor.
+      case ts.SyntaxKind.MethodDeclaration:
+        this.gatherMethod(node as ts.MethodDeclaration);
+        return false;  // Proceed with ordinary emit of the method.
+      case ts.SyntaxKind.Decorator:
+        // Skip emit of all decorators, as they are specially handled.
+        return true;
+      default:
+        return false;
+    }
+  }
+
+  /**
+   * emitMetadata emits the various gathered metadata, as static fields.
+   */
+  private emitMetadata() {
+    if (this.decorators) {
+      this.emit(`static decorators: DecoratorInvocation[] = [\n`);
+      for (let annotation of this.decorators) {
+        this.emitDecorator(annotation);
+        this.emit(',\n');
+      }
+      this.emit('];\n');
+    }
+
+    if (this.ctorParameters) {
+      this.emit(
+          `static ctorParameters: {type: new (...args) => any, decorators?: DecoratorInvocation[]}[] = [\n`);
+      for (let param of this.ctorParameters) {
+        if (!param) {
+          this.emit('null,\n');
+          continue;
+        }
+        let [ctor, decorators] = param;
+        this.emit(`{type: ${ctor}, `);
+        if (decorators) {
+          this.emit('decorators: [');
+          for (let decorator of decorators) {
+            this.emitDecorator(decorator);
+            this.emit(', ');
+          }
+          this.emit(']');
+        }
+        this.emit('},\n');
+      }
+      this.emit(`];\n`);
+    }
+
+    if (this.propDecorators) {
+      this.emit('static propDecorators: {[key: string]: DecoratorInvocation[]} = {\n');
+      for (let name of Object.keys(this.propDecorators)) {
+        this.emit(`'${name}': [`);
+        for (let decorator of this.propDecorators[name]) {
+          this.emitDecorator(decorator);
+          this.emit(',');
+        }
+        this.emit('],\n');
+      }
+      this.emit('};\n');
+    }
+  }
+
+  private emitDecorator(decorator: ts.Decorator) {
+    this.emit('{ type: ');
+    let expr = decorator.expression;
+    switch (expr.kind) {
+      case ts.SyntaxKind.Identifier:
+        // The decorator was a plain @Foo.
+        this.visit(expr);
+        break;
+      case ts.SyntaxKind.CallExpression:
+        // The decorator was a call, like @Foo(bar).
+        let call = expr as ts.CallExpression;
+        this.visit(call.expression);
+        if (call.arguments.length) {
+          this.emit(', args: [');
+          for (let arg of call.arguments) {
+            this.emit(arg.getText());
+            this.emit(', ');
+          }
+          this.emit(']');
+        }
+        break;
+      default:
+        this.errorUnimplementedKind(expr, 'gathering metadata');
+        this.emit('undefined');
+    }
+    this.emit(' }');
+  }
+}
+
+class DecoratorRewriter extends sickle.Rewriter {
+  process(): {output: string, diagnostics: ts.Diagnostic[]} {
+    this.visit(this.file);
+    return this.getOutput();
+  }
+
+  protected maybeProcess(node: ts.Node): boolean {
+    switch (node.kind) {
+      case ts.SyntaxKind.ClassDeclaration:
+        let {output, diagnostics} =
+            new ClassRewriter(this.file).process(node as ts.ClassDeclaration);
+        this.diagnostics.push(...diagnostics);
+        this.emit(output);
+        return true;
+      default:
+        return false;
+    }
+  }
+}
+
+export function convertDecorators(
+    fileName: string, sourceText: string): {output: string, diagnostics: ts.Diagnostic[]} {
+  let file = ts.createSourceFile(fileName, sourceText, ts.ScriptTarget.ES5, true);
+  return new DecoratorRewriter(file).process();
+}
---
+++ test/decorator-annotator_test.ts
@@ -0,0 +1,248 @@
+import * as ts from 'typescript';
+import {convertDecorators} from '../src/decorator-annotator';
+import {expect} from 'chai';
+import * as sickle from '../src/sickle';
+
+/**
+ * If true, attempt to compile all the test cases.
+ * Off by default because compilation takes a long time, and the test cases
+ * as currently produced are all valid.
+ */
+const checkCompilation = false;
+
+const testCaseFileName = 'testcase.ts';
+
+// If we verify the produced code compiles, this blob of code is some scaffolding
+// of some types and variables used in the test cases so the compiler likes it.
+const testSupportCode = `
+interface DecoratorInvocation {
+  type: Function;
+  args?: any[];
+}
+
+let Test1: Function;
+let Test2: Function;
+let Test3: Function;
+let Test4: Function;
+let Inject: Function;
+let param: any;
+class BarService {}
+`;
+
+function verifyCompiles(sourceText: string) {
+  let compilerOptions: ts.CompilerOptions = {
+    experimentalDecorators: true,
+    emitDecoratorMetadata: true,
+  };
+  let host = ts.createCompilerHost(compilerOptions);
+  let origGetSourceFile = host.getSourceFile;
+  host.getSourceFile = function(fileName: string, languageVersion: ts.ScriptTarget): ts.SourceFile {
+    if (fileName === testCaseFileName) {
+      sourceText = testSupportCode + sourceText;
+      return ts.createSourceFile(fileName, sourceText, languageVersion, true);
+    }
+    return origGetSourceFile(fileName, languageVersion);
+  };
+  let program = ts.createProgram([testCaseFileName], compilerOptions, host);
+  let errors = ts.getPreEmitDiagnostics(program);
+  if (errors.length > 0) {
+    console.error(sourceText);
+    console.error(sickle.formatDiagnostics(errors));
+  }
+}
+
+describe(
+    'decorator-annotator', () => {
+      function translate(sourceText: string, allowErrors = false) {
+        let {output, diagnostics} = convertDecorators(testCaseFileName, sourceText);
+        if (!allowErrors) expect(diagnostics).to.be.empty;
+        if (checkCompilation) verifyCompiles(output);
+        return {output, diagnostics};
+      }
+
+      describe('class decorator rewriter', () => {
+        it('leaves plain classes alone',
+           () => { expect(translate(`class Foo {}`).output).to.equal(`class Foo {}`); });
+
+        it('transforms decorated classes', () => {
+          expect(translate(`
+@Test1
+@Test2(param)
+class Foo {
+  field: string;
+}`).output).to.equal(`
+class Foo {
+  field: string;
+static decorators: DecoratorInvocation[] = [
+{ type: Test1 },
+{ type: Test2, args: [param, ] },
+];
+}`);
+        });
+
+        it('accepts various complicated decorators', () => {
+          expect(translate(`
+@Test1({name: 'percentPipe'}, class ZZZ {})
+@Test2
+@Test3()
+@Test4<T>(param)
+class Foo {
+}`).output).to.equal(`
+class Foo {
+static decorators: DecoratorInvocation[] = [
+{ type: Test1, args: [{name: 'percentPipe'}, class ZZZ {}, ] },
+{ type: Test2 },
+{ type: Test3 },
+{ type: Test4, args: [param, ] },
+];
+}`);
+        });
+
+        it(`doesn't eat 'export'`, () => {
+          expect(translate(`
+@Test1
+export class Foo {
+}`).output).to.equal(`
+export class Foo {
+static decorators: DecoratorInvocation[] = [
+{ type: Test1 },
+];
+}`);
+        });
+
+        it(`handles nested classes`, () => {
+          expect(translate(`
+@Test1
+export class Foo {
+  foo() {
+    @Test2
+    class Bar {
+    }
+  }
+}`).output).to.equal(`
+export class Foo {
+  foo() {
+    class Bar {
+    static decorators: DecoratorInvocation[] = [
+{ type: Test2 },
+];
+}
+  }
+static decorators: DecoratorInvocation[] = [
+{ type: Test1 },
+];
+}`);
+        });
+      });
+
+      describe('ctor decorator rewriter', () => {
+        it('ignores ctors that have no applicable injects', () => {
+          expect(translate(`
+class Foo {
+  constructor(bar: BarService, num: number) {
+  }
+}`).output).to.equal(`
+class Foo {
+  constructor(bar: BarService, num: number) {
+  }
+}`);
+        });
+
+        it('transforms injected ctors', () => {
+          expect(translate(`
+class Foo {
+  constructor(@Inject bar: BarService, num: number) {
+  }
+}`).output).to.equal(`
+class Foo {
+  constructor( bar: BarService, num: number) {
+  }
+static ctorParameters: {type: new (...args) => any, decorators?: DecoratorInvocation[]}[] = [
+{type: BarService, decorators: [{ type: Inject }, ]},
+null,
+];
+}`);
+        });
+
+        it('stores non annotated parameters if the class has at least one decorator', () => {
+          expect(translate(`
+@Test1()
+class Foo {
+  constructor(bar: BarService, num: number) {
+  }
+}`).output).to.equal(`
+class Foo {
+  constructor(bar: BarService, num: number) {
+  }
+static decorators: DecoratorInvocation[] = [
+{ type: Test1 },
+];
+static ctorParameters: {type: new (...args) => any, decorators?: DecoratorInvocation[]}[] = [
+{type: BarService, },
+null,
+];
+}`);
+        });
+
+        it('handles complex ctor parameters', () => {
+          expect(translate(`
+class Foo {
+  constructor(@Inject(param) x: BarService, {a, b}, defArg = 3, optional?: BarService) {
+  }
+}`).output).to.equal(`
+class Foo {
+  constructor( x: BarService, {a, b}, defArg = 3, optional?: BarService) {
+  }
+static ctorParameters: {type: new (...args) => any, decorators?: DecoratorInvocation[]}[] = [
+{type: BarService, decorators: [{ type: Inject, args: [param, ] }, ]},
+null,
+null,
+{type: BarService, },
+];
+}`);
+        });
+      });
+
+      describe('method decorator rewriter', () => {
+        it('leaves ordinary methods alone', () => {
+          expect(translate(`
+class Foo {
+  bar() {}
+}`).output).to.equal(`
+class Foo {
+  bar() {}
+}`);
+        });
+
+        it('gathers decorators from methods', () => {
+          expect(translate(`
+class Foo {
+  @Test1('somename')
+  bar() {}
+}`).output).to.equal(`
+class Foo {
+  bar() {}
+static propDecorators: {[key: string]: DecoratorInvocation[]} = {
+'bar': [{ type: Test1, args: ['somename', ] },],
+};
+}`);
+        });
+
+        it('errors on weird class members', () => {
+          let {diagnostics} = translate(
+              `
+class Foo {
+  @Test1('somename')
+  [param]() {}
+}`,
+              true /* allow errors */);
+
+          expect(sickle.formatDiagnostics(diagnostics))
+              .to.equal(
+                  'Error at testcase.ts:3:3: cannot process decorators on ComputedPropertyName');
+        });
+
+      });
+
+
+    });
