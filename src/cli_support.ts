1453d3439bd643a7e8373e81063176ee209aee80
Evan Martin <martine@danga.com>
2016-09-20 11:41:02 -0700

    Replace illegal characters when converting to goog.module (#207)

--- src/cli_support.ts
+++ src/cli_support.ts
@@ -2,8 +2,16 @@ import * as path from 'path';
 
 // Postprocess generated JS.
 export function pathToModuleName(context: string, fileName: string): string {
+  fileName = fileName.replace(/\.js$/, '');
+
   if (fileName[0] === '.') {
+    // './foo' or '../foo'.
+    // Resolve the path against the dirname of the current module.
     fileName = path.join(path.dirname(context), fileName);
   }
-  return fileName.replace(/\.js$/, '').replace(/\//g, '.');
+  // Replace characters not supported by goog.module.
+  let moduleName =
+      fileName.replace(/\//g, '.').replace(/^[^a-zA-Z_$]/, '_').replace(/[^a-zA-Z0-9._$]/g, '_');
+
+  return moduleName;
 }
