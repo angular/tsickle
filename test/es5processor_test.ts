74cdb04511781724b59804322aaee85411a9e6f7
Martin Probst <martin@probst.io>
2016-11-15 17:11:14 -0800

    Make the tsickle ES6 emit CommonJS compatible by excluding the exports object in the expected module.exports location.
    This helps if infrastructure ends up loading ES6 code to run in CommonJS.

--- test/es5processor_test.ts
+++ test/es5processor_test.ts
@@ -28,7 +28,9 @@ describe('convertCommonJsToGoogModule', () => {
     // NB: no line break added below.
     expectCommonJs('a.js', `console.log('hello');`, false)
         .to.equal(
-            `goog.module('a'); exports = {}; var module = {id: 'a.js'};console.log('hello');`);
+            `goog.module('a'); ` +
+            `exports = {}; var module = {id: 'a.js', exports: exports};` +
+            `console.log('hello');`);
   });
 
   it('adds a goog.module call to empty files', () => {
