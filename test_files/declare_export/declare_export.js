b2093cd8d96f68e328e8cdc5e951325e7ef9be69
Martin Probst <martin@probst.io>
2017-01-27 14:42:53 +0100

    Support exported {interface,class,function,variable} declarations.
    These already generated appropriate externs symbols, but did not
    generate a symbol on the exports object of the module, leading to
    Closure Compiler failures when importing the symbol.
    
    This code interprets exported deVclared symbols as global symbols that
    are accessible under the same name, consistent with previous code.
    That's somewhat odd though - we could equally well interpret it as
    symbols that the user promises should magically appear on the exports
    object. This does not really matter for Closure Compiler compatibility
    though.

---
+++ test_files/declare_export/declare_export.js
@@ -0,0 +1,6 @@
+goog.module('test_files.declare_export.declare_export');var module = module || {id: 'test_files/declare_export/declare_export.js'};
+exports.ExportDeclaredIf = ExportDeclaredIf;
+exports.exportedDeclaredVar = window.exportedDeclaredVar;
+exports.ExportDeclaredClass = ExportDeclaredClass;
+;
+exports.exportedDeclaredFn = exportedDeclaredFn;
---
+++ test_files/declare_export/declare_export.ts
@@ -0,0 +1,4 @@
+export declare interface ExportDeclaredIf { x: number; }
+export declare const exportedDeclaredVar: number;
+export declare class ExportDeclaredClass { x: number; };
+export declare function exportedDeclaredFn(): string;
