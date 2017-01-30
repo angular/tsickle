366eee0eaa1d3172138e5cfd6630581e97356965
Martin Probst <martin@probst.io>
2017-01-30 17:42:48 +0100

    Support variable declaration lists for declared exports. Support type definitions for declared exports.

--- test_files/declare_export/declare_export.js
+++ test_files/declare_export/declare_export.js
@@ -4,3 +4,6 @@ exports.exportedDeclaredVar = window.exportedDeclaredVar;
 exports.ExportDeclaredClass = ExportDeclaredClass;
 ;
 exports.exportedDeclaredFn = exportedDeclaredFn;
+exports.multiExportedDeclaredVar1 = window.multiExportedDeclaredVar1;
+exports.multiExportedDeclaredVar2 = window.multiExportedDeclaredVar2;
+exports.X = X;
