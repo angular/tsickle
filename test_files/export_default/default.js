c06282e71155328c0ccf0852da2274e3fd14ebba
Evan Martin <evanm@google.com>
2017-03-28 13:54:54 -0700

    ok

---
+++ test_files/export_default/default.js
@@ -0,0 +1,8 @@
+goog.module('test_files.export_default.default');var module = module || {id: 'test_files/export_default/default.js'};
+class Foo {
+    /**
+     * @return {number}
+     */
+    static bar() { return 3; }
+}
+exports.default = Foo;
