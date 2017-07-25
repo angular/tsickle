angular/tsickle

commit b298d95061cd19e344c6a9d75e72de7f74279879

Correctly emit downleveled decorators for combined pass
For: 
- getters
- for properties with comments.

Refactors and simplifies the emit logic.

Fixes bug that prevented closure types for
variable declarations as soon as the variable statement
had a jsdoc comment.

src/decorator-annotator.ts
@@ -317,40 +317,15 @@ class DecoratorRewriter extends Rewriter {
 
 export function visitClassContentIncludingDecorators(
     classDecl: ts.ClassDeclaration, rewriter: Rewriter, decoratorVisitor?: DecoratorClassVisitor) {
-  let pos = classDecl.getStart();
-  if (decoratorVisitor) {
-    // visit decorators if needed
-    ts.forEachChild(classDecl, child => {
-      if (child.kind !== ts.SyntaxKind.Decorator) {
-        return;
-      }
-      // Note: The getFullStart() of the first decorator is the same
-      // as the getFullStart() of the class declaration.
-      // Therefore, we need to use Math.max to not print the whitespace
-      // of the class again.
-      const childStart = Math.max(pos, child.getFullStart());
-      rewriter.writeRange(classDecl, pos, childStart);
-      if (decoratorVisitor.maybeProcessDecorator(child as ts.Decorator, childStart)) {
-        pos = child.getEnd();
-      }
-    });
+  if (rewriter.file.text[classDecl.getEnd() - 1] !== '}') {
+    rewriter.error(classDecl, 'unexpected class terminator');
   }
-  if (classDecl.members.length > 0) {
-    rewriter.writeRange(classDecl, pos, classDecl.members[0].getFullStart());
-    for (const member of classDecl.members) {
-      rewriter.visit(member);
-    }
-    pos = classDecl.getLastToken().getFullStart();
-  }
+  rewriter.writeNodeFrom(classDecl, classDecl.getStart(), classDecl.getEnd() - 1);
   // At this point, we've emitted up through the final child of the class, so all that
   // remains is the trailing whitespace and closing curly brace.
   // The final character owned by the class node should always be a '}',
   // or we somehow got the AST wrong and should report an error.
   // (Any whitespace or semicolon following the '}' will be part of the next Node.)
-  if (rewriter.file.text[classDecl.getEnd() - 1] !== '}') {
-    rewriter.error(classDecl, 'unexpected class terminator');
-  }
-  rewriter.writeRange(classDecl, pos, classDecl.getEnd() - 1);
   if (decoratorVisitor) {
     decoratorVisitor.emitMetadataAsStaticProperties();
   }

src/rewriter.ts
@@ -27,10 +27,10 @@ export abstract class Rewriter {
    */
   private indent = 0;
   /**
-   * Skip emitting any code before the given offset. Used to avoid emitting @fileoverview comments
-   * twice.
+   * Skip emitting any code before the given offset. E.g. used to avoid emitting @fileoverview
+   * comments twice.
    */
-  public skipUpToOffset = 0;
+  private skipUpToOffset = -1;
 
   constructor(public file: ts.SourceFile, private sourceMapper: SourceMapper = NOOP_SOURCE_MAPPER) {
   }
@@ -84,13 +84,19 @@ export abstract class Rewriter {
     this.writeNodeFrom(node, pos);
   }
 
-  writeNodeFrom(node: ts.Node, pos: number) {
+  writeNodeFrom(node: ts.Node, pos: number, end = Number.MAX_SAFE_INTEGER) {
+    if (node.getEnd() <= this.skipUpToOffset) {
+      return;
+    }
+    const oldSkipUpToOffset = this.skipUpToOffset;
+    this.skipUpToOffset = Math.max(this.skipUpToOffset, pos);
     ts.forEachChild(node, child => {
       this.writeRange(node, pos, child.getFullStart());
       this.visit(child);
       pos = child.getEnd();
     });
-    this.writeRange(node, pos, node.getEnd());
+    this.writeRange(node, pos, Math.min(end, node.getEnd()));
+    this.skipUpToOffset = oldSkipUpToOffset;
   }
 
   /**
@@ -100,7 +106,7 @@ export abstract class Rewriter {
    */
   writeRange(node: ts.Node, from: number, to: number) {
     from = Math.max(from, this.skipUpToOffset);
-    if (this.skipUpToOffset > 0 && to <= this.skipUpToOffset) {
+    if (from !== to && to <= this.skipUpToOffset) {
       return;
     }
     // Add a source mapping. writeRange(from, to) always corresponds to

src/tsickle.ts
@@ -525,6 +525,7 @@ class Annotator extends ClosureRewriter {
   private generatedExports = new Set<string>();
   /** DecoratorClassVisitor when lowering decorators while closure annotating */
   private currentDecoratorConverter: DecoratorClassVisitor|undefined;
+  private polymerBehaviorStackCount = 0;
 
   constructor(
       typeChecker: ts.TypeChecker, file: ts.SourceFile, host: AnnotatorHost,
@@ -659,7 +660,11 @@ class Annotator extends ClosureRewriter {
         // Only emit a type annotation when it's a plain variable and
         // not a binding pattern, as Closure doesn't(?) have a syntax
         // for annotating binding patterns.  See issue #128.
-        if (varDecl.name.kind === ts.SyntaxKind.Identifier) {
+        // Don't emit type annotation when the variable statement is a @polymerBehavior,
+        // as otherweise the polymer closure checker will fail.
+        // TODO(tbosch): file an issue for this!
+        if (this.polymerBehaviorStackCount === 0 &&
+            varDecl.name.kind === ts.SyntaxKind.Identifier) {
           this.emitJSDocType(varDecl);
         }
         return false;
@@ -684,15 +689,7 @@ class Annotator extends ClosureRewriter {
         this.emitFunctionType([ctor]);
         // Write the "constructor(...) {" bit, but iterate through any
         // parameters if given so that we can examine them more closely.
-        let offset = ctor.getStart();
-        if (ctor.parameters.length) {
-          for (const param of ctor.parameters) {
-            this.writeRange(node, offset, param.getFullStart());
-            this.visit(param);
-            offset = param.getEnd();
-          }
-        }
-        this.writeRange(node, offset, node.getEnd());
+        this.writeNodeFrom(ctor, ctor.getStart());
         return true;
       case ts.SyntaxKind.ArrowFunction:
         // It's difficult to annotate arrow functions due to a bug in
@@ -714,8 +711,7 @@ class Annotator extends ClosureRewriter {
         }
 
         this.emitFunctionType([fnDecl], tags);
-        this.writeRange(fnDecl, fnDecl.getStart(), fnDecl.body.getFullStart());
-        this.visit(fnDecl.body);
+        this.writeNodeFrom(fnDecl, fnDecl.getStart());
         return true;
       case ts.SyntaxKind.TypeAliasDeclaration:
         this.writeNode(node);
@@ -765,7 +761,14 @@ class Annotator extends ClosureRewriter {
         if (docTags.length > 0 && node.getFirstToken()) {
           this.emit('\n');
           this.emit(jsdoc.toString(docTags));
-          this.writeRange(node, node.getFirstToken().getStart(), node.getEnd());
+          const isPolymerBehavior = docTags.some(t => t.tagName === 'polymerBehavior');
+          if (isPolymerBehavior) {
+            this.polymerBehaviorStackCount++;
+          }
+          this.writeNodeFrom(node, node.getStart());
+          if (isPolymerBehavior) {
+            this.polymerBehaviorStackCount--;
+          }
           return true;
         }
         break;
@@ -840,19 +843,15 @@ class Annotator extends ClosureRewriter {
   }
 
   private handleSourceFile(sf: ts.SourceFile) {
-    this.emitSuppressChecktypes(sf);
-    for (const stmt of sf.statements) {
-      this.visit(stmt);
-    }
-    if (sf.statements.length) {
-      this.writeRange(sf, sf.statements[sf.statements.length - 1].getEnd(), sf.getEnd());
-    }
+    const start = this.emitSuppressChecktypes(sf);
+    this.writeNodeFrom(sf, start);
   }
 
   /**
    * Emits an \@suppress {checkTypes} fileoverview comment.
+   * Returns the place from where to start emitting the source file.
    */
-  private emitSuppressChecktypes(sf: ts.SourceFile) {
+  private emitSuppressChecktypes(sf: ts.SourceFile): number {
     const comments = ts.getLeadingCommentRanges(sf.getFullText(), 0) || [];
 
     let fileoverviewIdx = -1;
@@ -876,11 +875,10 @@ class Annotator extends ClosureRewriter {
         {tagName: 'suppress', type: 'checkTypes', text: 'checked by tsc'},
       ]));
       this.emit('\n');
-      return;
+      return sf.getFullStart();
     }
     const comment = comments[fileoverviewIdx];
     this.writeRange(sf, 0, comment.pos);
-    this.skipUpToOffset = comment.end;
 
     const parsed = jsdoc.parse(sf.getFullText().substring(comment.pos, comment.end));
     if (!parsed) throw new Error('internal error: JSDoc comment does not parse');
@@ -908,6 +906,7 @@ class Annotator extends ClosureRewriter {
     if (sf.getFullText().substring(comment.end, comment.end + 2) !== '\n\n') {
       this.emit('\n\n');  // separate from file body to avoid being dropped by tsc.
     }
+    return comment.end;
   }
 
   /**

test_files/decorator/decorator.decorated.ts
@@ -19,6 +19,12 @@ function classAnnotation(t: any) { return t; }
 class DecoratorTest {
   constructor(a: any[], n: number, b: boolean, promise: Promise<string>, arr: Array<string>, aClass: AClass, aClassWithGenerics: AClassWithGenerics<string>, aType: AType) {}
 
+  
+  get w(): number {
+    return 1;
+  }
+
+  /** Some comment */
   @decorator
   private x: number;
 
@@ -40,6 +46,7 @@ null, null,
 {type: AClassWithGenerics, },
 null,];
 static propDecorators: {[key: string]: {type: Function, args?: any[]}[]} = {
+"w": [{ type: annotationDecorator },],
 "y": [{ type: annotationDecorator },],
 };
 }

test_files/decorator/decorator.js
@@ -41,6 +41,12 @@ class DecoratorTest {
      * @param {!tsickle_forward_declare_1.AType} aType
      */
     constructor(a, n, b, promise, arr, aClass, aClassWithGenerics, aType) { }
+    /**
+     * @return {number}
+     */
+    get w() {
+        return 1;
+    }
 }
 DecoratorTest.decorators = [
     { type: classAnnotation },
@@ -56,6 +62,7 @@ DecoratorTest.ctorParameters = () => [
     null,
 ];
 DecoratorTest.propDecorators = {
+    "w": [{ type: annotationDecorator },],
     "y": [{ type: annotationDecorator },],
 };
 __decorate([
@@ -76,7 +83,10 @@ function DecoratorTest_tsickle_Closure_declarations() {
     DecoratorTest.ctorParameters;
     /** @type {!Object<string,!Array<{type: !Function, args: (undefined|!Array<?>)}>>} */
     DecoratorTest.propDecorators;
-    /** @type {number} */
+    /**
+     * Some comment
+     * @type {number}
+     */
     DecoratorTest.prototype.x;
     /** @type {number} */
     DecoratorTest.prototype.y;

test_files/decorator/decorator.ts
@@ -19,6 +19,12 @@ function classAnnotation(t: any) { return t; }
 class DecoratorTest {
   constructor(a: any[], n: number, b: boolean, promise: Promise<string>, arr: Array<string>, aClass: AClass, aClassWithGenerics: AClassWithGenerics<string>, aType: AType) {}
 
+  @annotationDecorator
+  get w(): number {
+    return 1;
+  }
+
+  /** Some comment */
   @decorator
   private x: number;
 

test_files/decorator/decorator.tsickle.ts
@@ -44,7 +44,16 @@ class DecoratorTest {
  */
 constructor(a: any[], n: number, b: boolean, promise: Promise<string>, arr: Array<string>, aClass: AClass, aClassWithGenerics: AClassWithGenerics<string>, aType: AType) {}
 
-  @decorator
+/**
+ * @return {number}
+ */
+get w(): number {
+    return 1;
+  }
+/**
+ * Some comment
+ */
+@decorator
 private x: number;
 private y: number;
 
@@ -63,6 +72,7 @@ null, null,
 {type: AClassWithGenerics, },
 null,];
 static propDecorators: {[key: string]: {type: Function, args?: any[]}[]} = {
+"w": [{ type: annotationDecorator },],
 "y": [{ type: annotationDecorator },],
 };
 }
@@ -77,7 +87,10 @@ DecoratorTest.decorators;
 DecoratorTest.ctorParameters;
 /** @type {!Object<string,!Array<{type: !Function, args: (undefined|!Array<?>)}>>} */
 DecoratorTest.propDecorators;
-/** @type {number} */
+/**
+ * Some comment
+ * @type {number}
+ */
 DecoratorTest.prototype.x;
 /** @type {number} */
 DecoratorTest.prototype.y;

test_files/jsdoc/jsdoc.js
@@ -109,4 +109,11 @@ function JSDocWithBadTag() { }
  * For example,
  * \@madeUpTag
  */
-const c = 'c';
+const /** @type {string} */ c = 'c';
+/**
+ * Don't emit type comments for Polymer behaviors,
+ * as this breaks their closure plugin :-(
+ *
+ * @polymerBehavior
+ */
+const somePolymerBehavior = {};

commit fa039d190c924f69a2864d27c9f287a28cf0add2

Only skip emitting text for comments.
The goal of the `skipUpToOffset` mechanism is to avoid emitting comments
that have already been processed. Commit b298d950 accidentally changed
this to skip emitting any text up to `skipUpToOffset`, by nesting the
skipping operation.

This meant that emitting nodes that preceded the
start offset of the current node in the source file would silently not
emit any text, which caused regression #547.

This commit changes the behaviour to only apply skipping for emit starts
that are within the trivia range of the given node, i.e. between
`getFullStart` and `getStart`. That achieves the goal of skipping
already emitted comments without accidentally ever skipping "real"
emitted text.

src/decorator-annotator.ts
@@ -136,14 +136,11 @@ export class DecoratorClassVisitor {
   /**
    * For lowering decorators, we need to refer to constructor types.
    * So we start with the identifiers that represent these types.
-   * However, TypeScript does not allow use to emit them in a value position
+   * However, TypeScript does not allow us to emit them in a value position
    * as it associated different symbol information with it.
    *
    * This method looks for the place where the value that is associated to
    * the type is defined and returns that identifier instead.
-   *
-   * @param typeSymbol
-   * @return The identifier
    */
   private getValueIdentifierForType(typeSymbol: ts.Symbol): ts.Identifier|null {
     if (!typeSymbol.valueDeclaration) {
@@ -252,6 +249,11 @@ export class DecoratorClassVisitor {
         } else {
           const [typeSymbol, typeStr] = ctor;
           let emitNode: ts.Identifier|null|undefined;
+          // For transformer mode, tsickle must emit not only the string referring to the type,
+          // but also create a source mapping, so that TypeScript can later recognize that the
+          // symbol is used in a value position, so that TypeScript does emit an import for the
+          // symbol.
+          // The code below and in getValueIdentifierForType finds a
           if (typeSymbol) {
             emitNode = this.getValueIdentifierForType(typeSymbol);
           }

src/rewriter.ts
@@ -30,7 +30,7 @@ export abstract class Rewriter {
    * Skip emitting any code before the given offset. E.g. used to avoid emitting @fileoverview
    * comments twice.
    */
-  private skipUpToOffset = -1;
+  private skipCommentsUpToOffset = -1;
 
   constructor(public file: ts.SourceFile, private sourceMapper: SourceMapper = NOOP_SOURCE_MAPPER) {
   }
@@ -93,18 +93,18 @@ export abstract class Rewriter {
   }
 
   writeNodeFrom(node: ts.Node, pos: number, end = node.getEnd()) {
-    if (end <= this.skipUpToOffset) {
+    if (end <= this.skipCommentsUpToOffset) {
       return;
     }
-    const oldSkipUpToOffset = this.skipUpToOffset;
-    this.skipUpToOffset = Math.max(this.skipUpToOffset, pos);
+    const oldSkipCommentsUpToOffset = this.skipCommentsUpToOffset;
+    this.skipCommentsUpToOffset = Math.max(this.skipCommentsUpToOffset, pos);
     ts.forEachChild(node, child => {
       this.writeRange(node, pos, child.getFullStart());
       this.visit(child);
       pos = child.getEnd();
     });
     this.writeRange(node, pos, end);
-    this.skipUpToOffset = oldSkipUpToOffset;
+    this.skipCommentsUpToOffset = oldSkipCommentsUpToOffset;
   }
 
   writeLeadingTrivia(node: ts.Node) {
@@ -121,9 +121,10 @@ export abstract class Rewriter {
    * node.getEnd().
    */
   writeRange(node: ts.Node, from: number, to: number) {
-    from = Math.max(from, this.skipUpToOffset);
-    if (from !== to && to <= this.skipUpToOffset) {
-      return;
+    const fullStart = node.getFullStart();
+    const textStart = node.getStart();
+    if (from >= fullStart && from < textStart) {
+      from = Math.max(from, this.skipCommentsUpToOffset);
     }
     // Add a source mapping. writeRange(from, to) always corresponds to
     // original source code, so add a mapping at the current location that

test_files/decorator_nested_scope/decorator_nested_scope.decorated.ts
@@ -1,0 +1,19 @@
+
+/** @Annotation */
+declare const Component: any;
+
+class SomeService {}
+
+export function main() {
+    
+    class TestComp3 {
+      constructor(service: SomeService) {}
+    static decorators: {type: Function, args?: any[]}[] = [
+{ type: Component },
+];
+/** @nocollapse */
+static ctorParameters: () => ({type: any, decorators?: {type: Function, args?: any[]}[]}|null)[] = () => [
+{type: SomeService, },
+];
+}
+}

test_files/decorator_nested_scope/decorator_nested_scope.js
@@ -1,0 +1,35 @@
+goog.module('test_files.decorator_nested_scope.decorator_nested_scope');var module = module || {id: 'test_files/decorator_nested_scope/decorator_nested_scope.js'};/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes} checked by tsc
+ */
+
+class SomeService {
+}
+/**
+ * @return {void}
+ */
+function main() {
+    class TestComp3 {
+        /**
+         * @param {!SomeService} service
+         */
+        constructor(service) { }
+    }
+    TestComp3.decorators = [
+        { type: Component },
+    ];
+    /** @nocollapse */
+    TestComp3.ctorParameters = () => [
+        { type: SomeService, },
+    ];
+    function TestComp3_tsickle_Closure_declarations() {
+        /** @type {!Array<{type: !Function, args: (undefined|!Array<?>)}>} */
+        TestComp3.decorators;
+        /**
+         * @nocollapse
+         * @type {function(): !Array<(null|{type: ?, decorators: (undefined|!Array<{type: !Function, args: (undefined|!Array<?>)}>)})>}
+         */
+        TestComp3.ctorParameters;
+    }
+}
+exports.main = main;

test_files/decorator_nested_scope/decorator_nested_scope.ts
@@ -1,0 +1,12 @@
+
+/** @Annotation */
+declare const Component: any;
+
+class SomeService {}
+
+export function main() {
+    @Component()
+    class TestComp3 {
+      constructor(service: SomeService) {}
+    }
+}

test_files/decorator_nested_scope/decorator_nested_scope.tsickle.ts
@@ -1,0 +1,38 @@
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes} checked by tsc
+ */
+
+
+/** @Annotation */
+declare const Component: any;
+class SomeService {}
+/**
+ * @return {void}
+ */
+export function main() {
+class TestComp3 {
+/**
+ * @param {!SomeService} service
+ */
+constructor(service: SomeService) {}
+    static decorators: {type: Function, args?: any[]}[] = [
+{ type: Component },
+];
+/** @nocollapse */
+static ctorParameters: () => ({type: any, decorators?: {type: Function, args?: any[]}[]}|null)[] = () => [
+{type: SomeService, },
+];
+}
+
+function TestComp3_tsickle_Closure_declarations() {
+/** @type {!Array<{type: !Function, args: (undefined|!Array<?>)}>} */
+TestComp3.decorators;
+/**
+ * @nocollapse
+ * @type {function(): !Array<(null|{type: ?, decorators: (undefined|!Array<{type: !Function, args: (undefined|!Array<?>)}>)})>}
+ */
+TestComp3.ctorParameters;
+}
+
+}

test_files/decorator_nested_scope/externs.js
@@ -1,0 +1,7 @@
+/**
+ * @externs
+ * @suppress {duplicate,checkTypes}
+ */
+// NOTE: generated by tsickle, do not edit.
+ /** @type {?} */
+var Component;