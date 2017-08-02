170bb99859736d3be6141a08069c5121c21abf2d
Evan Martin <martine@danga.com>
2017-08-02 12:06:58 -0700

    adjust enum test to be closer to reality (#569)
    With a single-element enum, in code like:
      let x: MyEnum.VALUE = MyEnum.VALUE;
    the type of x is just 'MyEnum'.  This might be a bug in TS
    but it's certainly the case that this test isn't trying to
    exercise that corner of the language.  Avoid it.
    
    This is more preliminary work for TypeScript 2.4.

--- test_files/enum_value_literal_type/enum_value_literal_type.js
+++ test_files/enum_value_literal_type/enum_value_literal_type.js
@@ -6,7 +6,9 @@ goog.module('test_files.enum_value_literal_type.enum_value_literal_type');var mo
 /** @enum {number} */
 const ExportedEnum = {
     VALUE: 0,
+    OTHERVALUE: 1,
 };
 exports.ExportedEnum = ExportedEnum;
 ExportedEnum[ExportedEnum.VALUE] = "VALUE";
+ExportedEnum[ExportedEnum.OTHERVALUE] = "OTHERVALUE";
 exports.x = ExportedEnum.VALUE;
