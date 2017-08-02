497165f14eec2ec019f15db24af0990b71fc810d
Evan Martin <evanm@google.com>
2017-08-02 11:57:00 -0700

    adjust enum test to be closer to reality
    With a single-element enum, in code like:
      let x: MyEnum.VALUE = MyEnum.VALUE;
    the type of x is just 'MyEnum'.  This might be a bug in TS
    but it's certainly the case that this test isn't trying to
    exercise that corner of the language.  Avoid it.
    
    This is more preliminary work for TypeScript 2.4.

--- test_files/enum_value_literal_type/enum_value_literal_type.ts
+++ test_files/enum_value_literal_type/enum_value_literal_type.ts
@@ -1,4 +1,10 @@
+// Note: if you only have one value in the enum, then the type of "x" below
+// is just ExportedEnum, regardless of the annotation.  This might be a bug
+// in TypeScript but this test is just trying to verify the behavior of
+// exporting an enum's value, not that.
+
 export enum ExportedEnum {
   VALUE = 0,
+  OTHERVALUE,
 }
 export const x: ExportedEnum.VALUE = ExportedEnum.VALUE;
--- test_files/enum_value_literal_type/enum_value_literal_type.tsickle.ts
+++ test_files/enum_value_literal_type/enum_value_literal_type.tsickle.ts
@@ -6,8 +6,10 @@
 
 /** @enum {number} */
 const ExportedEnum: DontTypeCheckMe = {
-  VALUE: 0,};
+  VALUE: 0,
+  OTHERVALUE: 1,};
 export {ExportedEnum};
 ExportedEnum[ExportedEnum.VALUE] = "VALUE";
+ExportedEnum[ExportedEnum.OTHERVALUE] = "OTHERVALUE";
 
 export const /** @type {ExportedEnum} */ x: ExportedEnum.VALUE = ExportedEnum.VALUE;
