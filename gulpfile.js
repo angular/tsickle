0232d762c43fc54ee3c478de403aa9f1001e3bad
Lucas Sloan <lucassloan@google.com>
2017-09-29 13:30:14 -0700

    Build and Test with Bazel (#623)
    Use bazel as the main build system instead of gulp. Now you can bazel test ...

--- gulpfile.js
+++ gulpfile.js
@@ -6,24 +6,14 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-require('source-map-support').install();
-
 var clangFormat = require('clang-format');
 var formatter = require('gulp-clang-format');
-var fs = require('fs');
 var gulp = require('gulp');
-var gutil = require('gulp-util');
-var merge = require('merge2');
-var mocha = require('gulp-mocha');
-var sourcemaps = require('gulp-sourcemaps');
-var ts = require('gulp-typescript');
 var tslint = require('gulp-tslint');
-var typescript = require('typescript');
 
-var tsProject = ts.createProject('tsconfig.json', {
-  // Specify the TypeScript version we're using.
-  typescript: typescript,
-});
+var onError = function(err) {
+  process.exit(1);
+};
 
 const formatted = ['*.js', 'src/**/*.ts', 'test/**/*.ts'];
 
@@ -45,74 +35,3 @@ gulp.task('test.check-lint', function() {
       .pipe(tslint.report())
       .on('warning', onError);
 });
-
-var hasError;
-var failOnError = true;
-
-var onError = function(err) {
-  hasError = true;
-  if (failOnError) {
-    process.exit(1);
-  }
-};
-
-gulp.task('compile', function() {
-  hasError = false;
-  var tsResult =
-      gulp.src(['src/**/*.ts']).pipe(sourcemaps.init()).pipe(tsProject()).on('error', onError);
-  return merge([
-    tsResult.dts.pipe(gulp.dest('built/definitions')),
-    // Write external sourcemap next to the js file
-    tsResult.js.pipe(sourcemaps.write('.', {includeContent: false, sourceRoot: '../../src'}))
-        .pipe(gulp.dest('built/src')),
-    tsResult.js.pipe(gulp.dest('built/src')),
-  ]);
-});
-
-gulp.task('test.compile', ['compile'], function(done) {
-  if (hasError) {
-    done();
-    return;
-  }
-  return gulp.src(['test/*.ts'], {base: '.'})
-      .pipe(sourcemaps.init())
-      .pipe(tsProject())
-      .on('error', onError)
-      .js.pipe(sourcemaps.write('.', {includeContent: false, sourceRoot: '../../test'}))
-      .pipe(gulp.dest('built/'));
-});
-
-gulp.task('test.unit', ['test.compile'], function(done) {
-  if (hasError) {
-    done();
-    return;
-  }
-  return gulp.src(['built/test/**/*.js', '!built/test/**/e2e*.js', '!built/test/**/golden*.js'])
-      .pipe(mocha({timeout: 1000}));
-});
-
-gulp.task('test.e2e', ['test.compile'], function(done) {
-  if (hasError) {
-    done();
-    return;
-  }
-  return gulp.src(['built/test/**/e2e*.js']).pipe(mocha({timeout: 25000}));
-});
-
-gulp.task('test.golden', ['test.compile'], function(done) {
-  if (hasError) {
-    done();
-    return;
-  }
-  return gulp.src(['built/test/**/golden*.js']).pipe(mocha({timeout: 1000}));
-});
-
-gulp.task('test', ['test.unit', 'test.e2e', 'test.golden', 'test.check-format', 'test.check-lint']);
-
-gulp.task('watch', function() {
-  failOnError = false;
-  gulp.start(['test.unit', 'test.golden']);  // Trigger initial build.
-  return gulp.watch(['src/**/*.ts', 'test/**/*.ts', 'test_files/**'], ['test.unit', 'test.golden']);
-});
-
-gulp.task('default', ['compile']);
