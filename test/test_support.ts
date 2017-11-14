angular/tsickle

commit 0232d762c43fc54ee3c478de403aa9f1001e3bad

Build and Test with Bazel (#623)
Use bazel as the main build system instead of gulp. Now you can bazel test ...

test/golden_tsickle_test.ts
@@ -92,12 +92,12 @@ function compareAgainstGolden(output: string|null, path: string) {
 }
 
 // Only run golden tests if we filter for a specific one.
-const testFn = TEST_FILTER ? describe.only : describe;
+const testFn = TEST_FILTER ? fdescribe : describe;
 
 testFn('golden tests with transformer', () => {
   testSupport.goldenTests().forEach((test) => {
     if (TEST_FILTER && !TEST_FILTER.testName.test(test.name)) {
-      it.skip(test.name);
+      xit(test.name);
       return;
     }
     let emitDeclarations = true;

test/test_support.ts
@@ -179,7 +179,7 @@ export class GoldenFileTest {
 }
 
 export function goldenTests(): GoldenFileTest[] {
-  const basePath = path.join(__dirname, '..', '..', 'test_files');
+  const basePath = path.join(process.env['RUNFILES'], 'io_angular_tsickle', 'test_files');
   const testNames = fs.readdirSync(basePath);
 
   const testDirs = testNames.map(testName => path.join(basePath, testName))

.bazelrc
@@ -1,0 +1,2 @@
+test --test_output=errors
+#TODO(alexeagle): enable worker mode

.circleci/config.yml
@@ -8,7 +8,7 @@ jobs:
       - checkout
       - restore_cache:
           key: tsickle-{{ checksum "yarn.lock" }}
-      - run: bazel run @yarn//:yarn
+      - run: yarn install # don't bazel's yarn, since yarn install calls bazel, and bazel can't reenter
       - run: bazel build ...
       - save_cache:
           key: tsickle-{{ checksum "yarn.lock" }}

.gitignore
@@ -1,6 +1,3 @@
-# Compiled JS
-/built
-
 # Dependency directory
 /node_modules
 

.travis.yml
@@ -1,10 +1,12 @@
 language: node_js
 sudo: false
+
 node_js:
   - "6"
-cache:
-  yarn: true
-  directories:
-    - node_modules
+cache: yarn
+install:
+  - ./travis_install.sh
+  - bazel version
+  - yarn install
 script:
   - yarn test

BUILD
@@ -3,3 +3,5 @@ exports_files(["tsconfig.json"])
 
 # NOTE: this will move to node_modules/BUILD in a later release
 filegroup(name = "node_modules", srcs = glob(["node_modules/**/*"]))
+
+filegroup(name = "test_files", srcs = glob(["test_files/**/*"]))

WORKSPACE
@@ -1,9 +1,11 @@
+workspace(name="io_angular_tsickle")
+
 load("@bazel_tools//tools/build_defs/repo:git.bzl", "git_repository")
 
 git_repository(
     name = "build_bazel_rules_nodejs",
     remote = "https://github.com/bazelbuild/rules_nodejs.git",
-    tag = "0.1.0",
+    tag = "0.1.8",
 )
 
 load("@build_bazel_rules_nodejs//:defs.bzl", "node_repositories")

commit 299bfd592224d89c40f2b331be45509f0be8bf2b

Restore e2e_test.
This test was previously broken in #623 when tsickle apparently somewhat
accidentally moved from the mocha test runner to jasmine.

Jasmine does not support an error parameter in the done callback, so
since then the result of the Closure tests was no longer checked.

test/e2e_clutz_dts_test.ts
@@ -19,7 +19,7 @@ import {compileWithTransfromer, createProgram, goldenTests} from './test_support
 
 describe('clutz dts', () => {
   it('produces a valid .d.ts', () => {
-    const tests = goldenTests().filter(t => /\.declaration\b/.test(t.name));
+    const tests = goldenTests().filter(t => t.isDeclarationTest);
 
     const dtsSources = new Map<string, string>();
 

test/e2e_test.ts
@@ -12,7 +12,7 @@ import * as closure from 'google-closure-compiler';
 import {goldenTests} from './test_support';
 
 export function checkClosureCompile(
-    jsFiles: string[], externsFiles: string[], done: (err?: Error) => void) {
+    jsFiles: string[], externsFiles: string[], done: DoneFn) {
   const startTime = Date.now();
   const total = jsFiles.length;
   if (!total) throw new Error('No JS files in ' + JSON.stringify(jsFiles));
@@ -20,6 +20,8 @@ export function checkClosureCompile(
   const CLOSURE_COMPILER_OPTS: closure.CompileOptions = {
     'checks_only': true,
     'jscomp_error': 'checkTypes',
+    // TODO(martinprobst): enable when NTI is ready. Currently doesn't support enum union types.
+    // 'new_type_inf': true,
     'warning_level': 'VERBOSE',
     'js': jsFiles,
     'externs': externsFiles,
@@ -30,17 +32,15 @@ export function checkClosureCompile(
   const compiler = new closure.compiler(CLOSURE_COMPILER_OPTS);
   compiler.run((exitCode, stdout, stderr) => {
     console.log('Closure compilation:', total, 'done after', Date.now() - startTime, 'ms');
-    if (exitCode !== 0) {
-      done(new Error(stderr));
-    } else {
+      expect(exitCode).toBe(0, stderr);
       done();
-    }
   });
 }
 
 describe('golden file tests', () => {
-  it('generates correct Closure code', (done: (err?: Error) => void) => {
-    const tests = goldenTests();
+  it('generates correct Closure code', (done) => {
+    // Declaration tests do not produce .js files.
+    const tests = goldenTests().filter(t => !t.isDeclarationTest);
     const goldenJs = ([] as string[]).concat(...tests.map(t => t.jsPaths));
     goldenJs.push('src/closure_externs.js');
     goldenJs.push('test_files/helpers.js');

test/golden_tsickle_test.ts
@@ -152,8 +152,8 @@ testFn('golden tests with transformer', () => {
         convertIndexImportShorthand: true,
         transformDecorators: true,
         transformTypesToClosure: true,
-        addDtsClutzAliases: /\.declaration\b/.test(test.name),
-        untyped: /\.untyped\b/.test(test.name),
+        addDtsClutzAliases: test.isDeclarationTest,
+        untyped: test.isUntypedTest,
         logWarning: (diag: ts.Diagnostic) => {
           let diags = diagnosticsByFile.get(diag.file!.fileName);
           if (!diags) {
@@ -187,11 +187,10 @@ testFn('golden tests with transformer', () => {
               Array.from(tsSources.keys())}`);
         }
       }
-      const isDeclarationTest = /\.declaration\b/.test(test.name);
       const {diagnostics, externs} = tsickle.emitWithTsickle(
           program, transformerHost, tsHost, tsCompilerOptions, targetSource,
           (fileName: string, data: string) => {
-            if (isDeclarationTest) {
+            if (test.isDeclarationTest) {
               // Only compare .d.ts files for declaration tests.
               if (!fileName.endsWith('.d.ts')) return;
             } else {
@@ -203,7 +202,7 @@ testFn('golden tests with transformer', () => {
             // we only care about the .d.ts files
             tscOutput[fileName] = data;
           });
-      if (!isDeclarationTest) {
+      if (!test.isDeclarationTest) {
         const sortedPaths = test.jsPaths.sort();
         const actualPaths = Object.keys(tscOutput).map(p => p.replace(/^\.\//, '')).sort();
         expect(sortedPaths).to.eql(actualPaths, `${test.jsPaths} vs ${actualPaths}`);

test/test_support.ts
@@ -173,6 +173,14 @@ export class GoldenFileTest {
         .map(f => path.join(this.path, GoldenFileTest.tsPathToJs(f)));
   }
 
+  get isDeclarationTest(): boolean {
+    return /\.declaration\b/.test(this.name);
+  }
+
+  get isUntypedTest(): boolean {
+    return /\.untyped\b/.test(this.name);
+  }
+
   /**
    * Find the absolute path to the tsickle root directory by reading the
    * symlink bazel puts into bazel-bin back into the test_files directory