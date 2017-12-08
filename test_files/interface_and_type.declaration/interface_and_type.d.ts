a368964ecf3e910bb395fd0a63cc7ad2b5c3c264
Lucas Sloan <lucas.sloan@gmail.com>
2017-12-08 09:07:54 -0800

    Emit clutz aliases in the style that incremental clutz expects.
    Global clutz runs allow symbols to be resolved to the name they have in their original contexts, even if they're exported under another name. Incremental clutz is forced to use the exporting name, so tsickle needs to emit both until global clutz goes away.

--- test_files/interface_and_type.declaration/interface_and_type.d.ts
+++ test_files/interface_and_type.declaration/interface_and_type.d.ts
@@ -6,4 +6,8 @@ declare global {
 		type module$contents$test_files$interface_and_type$declaration$interface_and_type_Foo = Foo;
 		type module$contents$test_files$interface_and_type$declaration$interface_and_type_Bar = Bar;
 	}
+	namespace ಠ_ಠ.clutz.module$exports$test_files$interface_and_type$declaration$interface_and_type {
+		type Foo = module$contents$test_files$interface_and_type$declaration$interface_and_type_Foo;
+		type Bar = module$contents$test_files$interface_and_type$declaration$interface_and_type_Bar;
+	}
 }
--- test_files/nested_folder.declaration/inner_folder/nested.d.ts
+++ test_files/nested_folder.declaration/inner_folder/nested.d.ts
@@ -3,4 +3,7 @@ declare global {
 	namespace ಠ_ಠ.clutz {
 		const module$contents$test_files$nested_folder$declaration$inner_folder$nested_x: typeof x;
 	}
+	namespace ಠ_ಠ.clutz.module$exports$test_files$nested_folder$declaration$inner_folder$nested {
+		const x: typeof module$contents$test_files$nested_folder$declaration$inner_folder$nested_x;
+	}
 }
