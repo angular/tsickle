b3daf28652012f1e0fa3c369ca372f533a5042bb
Alex Eagle <alexeagle@google.com>
2018-01-09 19:28:19 -0800

    upgrade bazel to 0.9

--- travis_install.sh
+++ travis_install.sh
@@ -1,7 +1,7 @@
 # installs bazel so travis can build and run tests
 mkdir tmp
 cd tmp
-BAZEL_VERSION=0.5.4
+BAZEL_VERSION=0.9.0
 curl --location --compressed "https://github.com/bazelbuild/bazel/releases/download/$BAZEL_VERSION/bazel-$BAZEL_VERSION-installer-linux-x86_64.sh" > "bazel-$BAZEL_VERSION-installer-linux-x86_64.sh"
 chmod +x "bazel-$BAZEL_VERSION-installer-linux-x86_64.sh"
 "./bazel-$BAZEL_VERSION-installer-linux-x86_64.sh" --user
