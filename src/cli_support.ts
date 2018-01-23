3706db109f87c55d28fdd02486a5ae176255d460
Robin Keller <robink@zoosk.com>
2018-01-23 10:23:15 -0800

    Fix goog.module naming consistency by ensuring absolute paths are used everywhere

--- src/cli_support.ts
+++ src/cli_support.ts
@@ -10,13 +10,17 @@ import * as path from 'path';
 
 // Postprocess generated JS.
 export function pathToModuleName(context: string, fileName: string): string {
-  fileName = fileName.replace(/\.js$/, '');
+  fileName = fileName.replace(/\.[tj]s$/, '');
 
   if (fileName[0] === '.') {
     // './foo' or '../foo'.
     // Resolve the path against the dirname of the current module.
     fileName = path.join(path.dirname(context), fileName);
   }
+
+  // Ensure consistency by naming all modules after their absolute paths
+  fileName = path.resolve(fileName);
+
   // Replace characters not supported by goog.module.
   const moduleName =
       fileName.replace(/\/|\\/g, '.').replace(/^[^a-zA-Z_$]/, '_').replace(/[^a-zA-Z0-9._$]/g, '_');
