d2fc14c2d0516fb14806c13bcef26241b99b8671
Evan Martin <evan.martin@gmail.com>
2018-04-09 16:28:40 -0700

    extend the declare_export test to showcase a bug
    This doesn't change any behavior, but extends the test suite
    to include some more comments about a particular bug.  This will
    make the diff from the fix easier to read.
    
    A step on #352.

--- test_files/declare_export/declare_export.js
+++ test_files/declare_export/declare_export.js
@@ -2,14 +2,29 @@
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
  */
-goog.module('test_files.declare_export.declare_export');var module = module || {id: 'test_files/declare_export/declare_export.js'};
+goog.module('test_files.declare_export.declare_export');var module = module || {id: 'test_files/declare_export/declare_export.js'};// All of the types/values declared in this file should
+// 1) generate externs
+// 2) generate an export
+// 3) be usable within the file
+//
+// TODO(https://github.com/angular/tsickle/issues/352):
+// For types, we have the additional requirement that the externs name
+// should be namespaced into a private namespace.
+// E.g. "export declare interface Error" should not conflict with the
+// Closure builtin Error type.
+
 /** @typedef {ExportDeclaredIf} */
 exports.ExportDeclaredIf;
+let /** @type {!ExportDeclaredIf} */ user1;
 exports.exportedDeclaredVar = window.exportedDeclaredVar;
+let /** @type {number} */ user2 = exports.exportedDeclaredVar;
 exports.ExportDeclaredClass = ExportDeclaredClass;
-;
+let /** @type {!ExportDeclaredClass} */ user3;
 exports.exportedDeclaredFn = exportedDeclaredFn;
+let /** @type {function(): string} */ user4 = exportedDeclaredFn;
 exports.multiExportedDeclaredVar1 = window.multiExportedDeclaredVar1;
 exports.multiExportedDeclaredVar2 = window.multiExportedDeclaredVar2;
+let /** @type {string} */ user5 = exports.multiExportedDeclaredVar1;
 /** @typedef {X} */
 exports.X;
+let /** @type {string} */ user6;
