c4abb1785d502d165d76df66e624e2c4dd0093c2
Martin Probst <martin@probst.io>
2018-05-11 14:00:35 +0200

    Add a test for non-null generics.
    In Closure Compiler, generic types (`T`) are always nullable, with no
    straight forward syntax to express a non-nullable generic type (*).
    Normally this code would produce a warning, but the `@suppress
    {checkTypes}` prevents that successfully.
    
    This change is a pure regression test to make sure this stays the same.
    
    (*) Closure Compiler's Type Transformation Language (TTL) allows
    expressing the concept, but leads to very cumbersome types:
    
        /**
        * @template T
        * @param {(null|T)} value
        * @param {!TNotNull} def
        * @return {!TNotNull}
        * @template TNotNull :=
        *     cond(isUnknown(T), unknown(),
        *       mapunion(T, (X) =>
        *         cond(eq(X, 'null'), none(), X)))
        * =:
        */
        function getOrDefault(value, def) {
            return def;
        }
    
    Given the error is successfully repressed, this doesn't seem worth
    implementing in tsickle.

---
+++ test_files/nonnull_generics/nonnull_generics.js
@@ -0,0 +1,29 @@
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes} checked by tsc
+ */
+goog.module('test_files.nonnull_generics.nonnull_generics');
+var module = module || { id: 'test_files/nonnull_generics/nonnull_generics.ts' };
+/**
+ * getOrDefault removes the |null branch from its input. In TypeScript, this works, but in Closure,
+ * generics like T are always nullable, and there's no syntax to specify a non-nullable generic.
+ * Thus, in Closure, `getOrDefault(maybeNull, 'goodbye')` ends up still being string|null.
+ * @template T
+ * @param {(null|T)} value
+ * @param {T} def
+ * @return {T}
+ */
+function getOrDefault(value, def) {
+    return value || def;
+}
+/**
+ * @return {string}
+ */
+function getMsg() {
+    const /** @type {(null|string)} */ maybeNull = Math.random() > 0.5 ? 'hello' : null;
+    // The value below is inferred as string|null, which would normally cause Closure Compiler to
+    // complain about an inconsistent nullable return type. However the @suppress {checkTypes}
+    // inserted by tsickle at the top will supprress that error.
+    return getOrDefault(maybeNull, 'goodbye');
+}
+console.log(getMsg());
---
+++ test_files/nonnull_generics/nonnull_generics.ts
@@ -0,0 +1,18 @@
+/**
+ * getOrDefault removes the |null branch from its input. In TypeScript, this works, but in Closure,
+ * generics like T are always nullable, and there's no syntax to specify a non-nullable generic.
+ * Thus, in Closure, `getOrDefault(maybeNull, 'goodbye')` ends up still being string|null.
+ */
+function getOrDefault<T>(value: T|null, def: T) {
+  return value || def;
+}
+
+function getMsg(): string {
+  const maybeNull: string|null = Math.random() > 0.5 ? 'hello' : null;
+  // The value below is inferred as string|null, which would normally cause Closure Compiler to
+  // complain about an inconsistent nullable return type. However the @suppress {checkTypes}
+  // inserted by tsickle at the top will supprress that error.
+  return getOrDefault(maybeNull, 'goodbye');
+}
+
+console.log(getMsg());
