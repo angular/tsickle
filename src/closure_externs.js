4bbbb4dec9f4b3abb1837c20f98139fb0cba7f4f
Lucas Sloan <lucassloan@google.com>
2018-05-15 16:52:46 -0700

    Add web animations typings to closure_externs.js.
    The typings for web animations were just added to the typescript standard library.  They partially replace the DefinitelyTyped typings for web animations.  The closure platform externs don't include web animations, which was previously fine, since tsickle produced the externs from the DefinitelyTyped typings.  With 2.8, many of the typings are in the standard library, so closure_externs.js needs to contain the corresponding externs.

--- src/closure_externs.js
+++ src/closure_externs.js
@@ -78,3 +78,168 @@ var PromiseConstructorLike;
 /** @typedef {function(new:symbol)} */
 var SymbolConstructor;
 
+/**
+ * @constructor
+ * @param {(undefined|!AnimationEffectReadOnly)=} effect
+ * @param {(undefined|!AnimationTimeline)=} timeline
+ */
+var Animation = function(effect, timeline) {}
+/** @type {(null|number)} */
+Animation.prototype.currentTime;
+/** @type {!AnimationEffectReadOnly} */
+Animation.prototype.effect;
+/** @type {!Promise<!Animation>} */
+Animation.prototype.finished;
+/** @type {string} */
+Animation.prototype.id;
+/** @type {boolean} */
+Animation.prototype.pending;
+/** @type {string} */
+Animation.prototype.playState;
+/** @type {number} */
+Animation.prototype.playbackRate;
+/** @type {!Promise<!Animation>} */
+Animation.prototype.ready;
+/** @type {number} */
+Animation.prototype.startTime;
+/** @type {!AnimationTimeline} */
+Animation.prototype.timeline;
+/** @type {function(this: (!Animation), !AnimationPlaybackEvent): ?} */
+Animation.prototype.oncancel;
+/** @type {function(this: (!Animation), !AnimationPlaybackEvent): ?} */
+Animation.prototype.onfinish;
+
+/**
+ * @return {void}
+ */
+Animation.prototype.cancel = function() {};
+
+/**
+ * @return {void}
+ */
+Animation.prototype.finish = function() {};
+
+/**
+ * @return {void}
+ */
+Animation.prototype.pause = function() {};
+
+/**
+ * @return {void}
+ */
+Animation.prototype.play = function() {};
+
+/**
+ * @return {void}
+ */
+Animation.prototype.reverse = function() {};
+
+/**
+ * @record
+ * @struct
+ */
+function AnimationEffectReadOnly() {}
+/** @type {number} */
+AnimationEffectReadOnly.prototype.timing;
+
+/**
+ * @return {!ComputedTimingProperties}
+ */
+AnimationEffectReadOnly.prototype.getComputedTiming = function() {};
+/**
+ * @extends {EventInit}
+ * @record
+ * @struct
+ */
+function AnimationEventInit() {}
+/** @type {(undefined|number)} */
+AnimationEventInit.prototype.animationName;
+/** @type {(undefined|number)} */
+AnimationEventInit.prototype.elapsedTime;
+/**
+ * @constructor
+ * @param {string} typeArg
+ * @param {(undefined|!AnimationEventInit)=} eventInitDict
+ */
+var AnimationEvent = function(typeArg, eventInitDict) {}
+/** @type {string} */
+AnimationEvent.prototype.animationName;
+/** @type {number} */
+AnimationEvent.prototype.elapsedTime;
+
+/**
+ * @record
+ * @struct
+ */
+function AnimationKeyFrame() {}
+/** @type {(undefined|string|!Array<string>)} */
+AnimationKeyFrame.prototype.easing;
+/** @type {(undefined|null|number|!Array<(null|number)>)} */
+AnimationKeyFrame.prototype.offset;
+
+/**
+ * @record
+ * @struct
+ */
+function AnimationOptions() {}
+/** @type {(undefined|number)} */
+AnimationOptions.prototype.delay;
+/** @type {(undefined|string)} */
+AnimationOptions.prototype.direction;
+/** @type {(undefined|number)} */
+AnimationOptions.prototype.duration;
+/** @type {(undefined|string)} */
+AnimationOptions.prototype.easing;
+/** @type {(undefined|number)} */
+AnimationOptions.prototype.endDelay;
+/** @type {(undefined|string)} */
+AnimationOptions.prototype.fill;
+/** @type {(undefined|string)} */
+AnimationOptions.prototype.id;
+/** @type {(undefined|number)} */
+AnimationOptions.prototype.iterationStart;
+/** @type {(undefined|number)} */
+AnimationOptions.prototype.iterations;
+/**
+ * @constructor
+ * @param {string} type
+ * @param {(undefined|!AnimationPlaybackEventInit)=} eventInitDict
+ */
+var AnimationPlaybackEvent = function(type, eventInitDict) {}
+/** @type {(null|number)} */
+AnimationPlaybackEvent.prototype.currentTime;
+/** @type {(null|number)} */
+AnimationPlaybackEvent.prototype.timelineTime;
+
+/**
+ * @extends {EventInit}
+ * @record
+ * @struct
+ */
+function AnimationPlaybackEventInit() {}
+/** @type {(undefined|null|number)} */
+AnimationPlaybackEventInit.prototype.currentTime;
+/** @type {(undefined|null|number)} */
+AnimationPlaybackEventInit.prototype.timelineTime;
+/**
+ * @record
+ * @struct
+ */
+function AnimationTimeline() {}
+/** @type {(null|number)} */
+AnimationTimeline.prototype.currentTime;
+/**
+ * @record
+ * @struct
+ */
+function ComputedTimingProperties() {}
+/** @type {number} */
+ComputedTimingProperties.prototype.activeDuration;
+/** @type {(null|number)} */
+ComputedTimingProperties.prototype.currentIteration;
+/** @type {number} */
+ComputedTimingProperties.prototype.endTime;
+/** @type {(null|number)} */
+ComputedTimingProperties.prototype.localTime;
+/** @type {(null|number)} */
+ComputedTimingProperties.prototype.progress;
