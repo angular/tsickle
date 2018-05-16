e16ca4631e871d93078b86eb8cb93e86e6cd8920
Martin Probst <martin@probst.io>
2018-05-16 17:57:28 +0200

    Upgrade to bazel 0.13.0 on travis.

--- travis_install.sh
+++ travis_install.sh
@@ -2,7 +2,7 @@
 mkdir tmp
 cd tmp
 # Update the check in WORKSPACE when upgrading Bazel.
-BAZEL_VERSION=0.9.0
+BAZEL_VERSION=0.13.0
 curl --location --compressed "https://github.com/bazelbuild/bazel/releases/download/$BAZEL_VERSION/bazel-$BAZEL_VERSION-installer-linux-x86_64.sh" > "bazel-$BAZEL_VERSION-installer-linux-x86_64.sh"
 chmod +x "bazel-$BAZEL_VERSION-installer-linux-x86_64.sh"
 "./bazel-$BAZEL_VERSION-installer-linux-x86_64.sh" --user
