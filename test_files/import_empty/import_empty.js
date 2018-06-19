e91f57a4d53e2924a7ee0c8b7f31bdc6708ebdc1
Martin Probst <martin@probst.io>
2018-06-19 14:07:13 +0200

    Fix TS 2.9 bailing on import from a non-module.
    This introduces an emport export, which causes a forward declaration
    that also side-effect imports the file. That however does not invalidate
    the test, which checks a different code path, and just asserts tsickle
    does not crash on an empty set of imports.

--- test_files/import_empty/import_empty.js
+++ test_files/import_empty/import_empty.js
@@ -1,7 +1,9 @@
 /**
- * @fileoverview added by tsickle
- * @suppress {checkTypes} checked by tsc
+ * @fileoverview Make sure tsickle does not crash on empty imports.
+ * @suppress {checkTypes,extraRequire} checked by tsc
  */
 goog.module('test_files.import_empty.import_empty');
 var module = module || { id: 'test_files/import_empty/import_empty.ts' };
+const tsickle_forward_declare_1 = goog.forwardDeclare("test_files.import_empty.imported");
+goog.require("test_files.import_empty.imported"); // force type-only module to be loaded
 console.log('hello');
--- test_files/import_empty/import_empty.ts
+++ test_files/import_empty/import_empty.ts
@@ -1,3 +1,5 @@
+/** @fileoverview Make sure tsickle does not crash on empty imports. */
+
 import {} from './imported';
 
 console.log('hello');
