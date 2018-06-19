070aba8e847fc0e7629f0b8cd6db72ac60438565
Martin Probst <martin@probst.io>
2018-06-19 14:43:29 +0200

    Reinstate the E2E closure test.
    During the last `BUILD` verschlimmbesserung, the `closure_e2e_test`
    Jasmine test ended up depending on the `e2e_test_lib`, not
    `closure_e2e_test_lib` as intended, causing the Closure test to never
    execute.
    
    On the bright side, this executed all other E2E tests twice, for added
    safety. Sigh. The moral is that we should not be trusted around test
    infrastructure.
    
    This also removes the `duplicated_externs` test, which does not work
    with the Closure version used in tsickle.

--- test_files/duplicated_externs.js
+++
@@ -1,9 +0,0 @@
-/**
- * @fileoverview A file containing externs duplicating the symbols generated from
- * declare/declare.d.ts (and thus declare/externs.js). This is used to ensure generated externs do
- * not conflict with pre-existing externs.
- * @externs
- */
-
-/** @type {number} */
-var declareGlobalVar;
