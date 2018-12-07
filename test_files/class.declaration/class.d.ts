8ea0dd291655b8ab93c0284616c615cc79006464
Rado Kirov <rkirov@gmail.com>
2018-12-06 18:22:21 -0800

    Remove _Instance redirections for clutz symbols.
    Since
    https://github.com/angular/clutz/commit/b86a51c6ec5d910c7d7b177fcefe8c3c1ae28eb8
    clutz no longer splits classes into _Instance and static side.

--- test_files/class.declaration/class.d.ts
+++ test_files/class.declaration/class.d.ts
@@ -3,10 +3,8 @@ export declare class Foo {
 declare global {
 	namespace ಠ_ಠ.clutz {
 		export {Foo as module$contents$test_files$class$declaration$class_Foo}
-		export {Foo as module$contents$test_files$class$declaration$class_Foo_Instance}
 	}
 	namespace ಠ_ಠ.clutz.module$exports$test_files$class$declaration$class {
 		export {module$contents$test_files$class$declaration$class_Foo as Foo}
-		export {module$contents$test_files$class$declaration$class_Foo as Foo_Instance}
 	}
 }
