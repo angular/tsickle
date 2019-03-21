3b696bea8da5dbe88546cf3fb717eee2676f4c62
Erik Welander <erik@welander.us>
2019-03-21 16:16:35 -0700

    Update overload_with_this_parameter.d.ts

--- test_files/overload_with_this_parameter/overload_with_this_parameter.d.ts
+++ test_files/overload_with_this_parameter/overload_with_this_parameter.d.ts
@@ -1,5 +1,7 @@
-interface Foo<T> {
-  // Not all signatures have a this parameter.
-  bar<U>(this: U[], x: 1): U[];
-  bar<U>(x?: number): any[];
+interface Foo {
+  // Not all signatures have a this parameter. Should still generate proper
+  // jsdoc.
+  bar(this: Foo, x: boolean, y: number): boolean;
+  bar(this: void, x: string): string;
+  bar(x?: number): any[];
 }
