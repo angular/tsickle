22d20dfd7ade03903649e78e89169486ac2e6d6a
Erik Welander <erik@welander.us>
2019-03-21 16:09:44 -0700

    Update overload_with_this_parameter.d.ts

--- test_files/overload_with_this_parameter/overload_with_this_parameter.d.ts
+++ test_files/overload_with_this_parameter/overload_with_this_parameter.d.ts
@@ -1,7 +1,5 @@
-interface Foobar<T> {
-  flat<U>(this: U[][][], depth: 2): U[];
-  flat<U>(this: U[][], depth?: 1): U[];
-  flat<U>(this: U[], depth: 0): U[];
-  /** @param depth The maximum recursion depth */
-  flat<U>(depth?: number): any[];
+interface Foo<T> {
+  // Not all signatures have a this parameter.
+  bar<U>(this: U[], x: 1): U[];
+  bar<U>(x?: number): any[];
 }
