653e61e39310563604a026b03ba010ef92ab0318
Ayaz Hafiz <ayaz.hafiz.1@gmail.com>
2019-07-15 08:37:21 -0700

    fix(symbol): create closure type declaration for computed function names
    Functions with computed symbol names are not processed because their
    names are not strings. Resolve this by handling for the specific case of
    computed name in a function declaration and creating and element access
    rather than a property access for the symbol naming the function.
    
    Issue close #913.

--- test_files/symbol/symbol.ts
+++ test_files/symbol/symbol.ts
@@ -1,7 +1,11 @@
-const uniqueSymbol = Symbol('my symbol');
+const uniqueSymbol: unique symbol = Symbol('my symbol');
 
 function usingSymbol(symbolTyped: symbol) {
   console.log(symbolTyped);
+}
+
+interface ComputedSymbol {
+  [uniqueSymbol](): number;
 }
 
 usingSymbol(uniqueSymbol);
