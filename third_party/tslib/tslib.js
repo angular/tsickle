351d7a2e8f39cadfdbbcdb5e0ce4f9e0aedecef4
theseanl <i73hi64d0wr5df8pckig@gmail.com>
2019-09-25 18:40:33 +0000

    fix type annotation for __rest

--- third_party/tslib/tslib.js
+++ third_party/tslib/tslib.js
@@ -58,7 +58,7 @@ exports.__rest = function (s, e) {
     for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
         t[p] = s[p];
     if (s != null && typeof Object.getOwnPropertySymbols === "function")
-        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) if (e.indexOf(p[i]) < 0 && (/** @type {function((string|symbol)):boolean} */ (Object.prototype.propertyIsEnumerable)).call(s, p[i]))
             t[p[i]] = s[p[i]];
     return t;
 };
