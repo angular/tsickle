adc27ee1be66f275c643a4ef59cf00dd75ded7a8
Evan Martin <evan.martin@gmail.com>
2019-09-30 11:58:05 -0700

    add more async test cases, simplify code
    These additional cases came from some problematic google3 code,
    as well as from Rado's reverted patch.
    
    Also, we don't need any Promises to exercise the this-checking logic,
    so remove them to make the tests easier to read.

--- test_files/async_functions/async_functions.ts
+++ test_files/async_functions/async_functions.ts
@@ -1,26 +1,26 @@
+/**
+ * Exercises various forms of async functions.  When TypeScript downlevels these functions, it
+ * inserts a reference to 'this' which then tickles a Closure check around whether 'this' has a
+ * known type.
+ */
+
 export {};
 
 async function asyncTopLevelFunction(param: string): Promise<string> {
-  const p = new Promise<string>((res, rej) => {
-    res(param);
-  });
-  const s = await p;
+  const s = await 'a';
   return s;
 }
 
+// Note: some Closure checks are only triggered when functions are exported.
+export {asyncTopLevelFunction, asyncTopLevelFunctionWithThisType};
+
 async function asyncTopLevelFunctionWithThisType(this: Container, param: string): Promise<string> {
-  const p = new Promise<string>((res, rej) => {
-    res(param);
-  });
-  const s = await p;
+  const s = await 3;
   return s + this.field;
 }
 
 const asyncTopLevelArrowFunction = async(param: string): Promise<string> => {
-  const p = new Promise<string>((res, rej) => {
-    res(param);
-  });
-  const s = await p;
+  const s = await 3;
   return s + this.field;
 };
 
@@ -34,18 +34,26 @@ class Container {
 
   containerMethod() {
     const asyncArrowFunctionInMethod = async(param: string): Promise<string> => {
-      const p = new Promise<string>((res, rej) => {
-        res(param);
-      });
-      const s = await p;
+      const s = await 3;
       return s + this.field;
     };
     async function asyncFunctionInMethod(param: string): Promise<string> {
-      const p = new Promise<string>((res, rej) => {
-        res(param);
-      });
-      const s = await p;
+      const s = await 3;
       return s + this.field;
     }
   }
+
+  static staticField = 's';
+
+  static async asyncStaticMethod() {
+    const s = await asyncTopLevelFunction('x');
+    return s + this.staticField;
+  }
+}
+
+const asyncFnExpression = async function f() {};
+const asyncArrowFn = async () => {};
+
+export function toplevelContainingAsync() {
+  return async () => 3;
 }
