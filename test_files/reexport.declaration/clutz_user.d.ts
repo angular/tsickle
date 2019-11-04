82d37bf6bd3cf72a18376177ffa22df26526212c
Martin Probst <martin@probst.io>
2019-11-04 10:23:21 +0100

    Fix renamed exports in clutz aliases.
    For a renamed export, previously we'd incorrectly emit the local, not
    the exported, name in the "module exports" name.

---
+++ test_files/reexport.declaration/clutz_user.d.ts
@@ -0,0 +1,9 @@
+/** @fileoverview Tests that renamed exports get emitted with the correct clutz alias names. */
+
+export {};
+
+declare const moduleInternalName:
+    typeof ಠ_ಠ.clutz.module$contents$test_files$reexport$declaration$renamed_export_MOTHER;
+
+declare const exportedName:
+    typeof ಠ_ಠ.clutz.module$exports$test_files$reexport$declaration$renamed_export.MOTHER;
