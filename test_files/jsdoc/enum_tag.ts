f49cce90e6aa3ba944736744ffbe3438567da2a1
Ryan Delaney <rdel@google.com>
2019-11-25 11:27:39 -0800

    Visit enum nodes in jsdocTransformer to validate the enum's JSDoc (#1114)

---
+++ test_files/jsdoc/enum_tag.ts
@@ -0,0 +1,34 @@
+/**
+ * @fileoverview Checks that JSDoc `@enum` tags on an `enum` are flagged as
+ * warnings.
+ */
+
+/**
+ * @enum
+ */
+enum A {
+  A,
+  B
+}
+
+/** @enum */
+enum B {
+  A,
+  B
+}
+
+/**
+ * @enum {number}
+ */
+enum C {
+  A = 10,
+  B = 20
+}
+
+class SomeComponent {
+  /** @enum {number} */
+  readonly MY_ENUM = {
+    A: 10,
+    B: 20,
+  };
+}
