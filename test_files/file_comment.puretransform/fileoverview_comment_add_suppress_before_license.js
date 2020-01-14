b095e4d5d78e5ed31ba5c168fcaff03c93789fa2
Yiting Wang <yitingwang@google.com>
2020-01-14 14:54:09 -0800

    Special case the @license tag in the file overview comments so that newly added @suppress tags are placed above @license tags.
    @license tag treats all text that comes after it as part of the license, so if @suppress is added after @license tags, it is essentially ignored by the Closure Compiler.

---
+++ test_files/file_comment.puretransform/fileoverview_comment_add_suppress_before_license.js
@@ -0,0 +1,11 @@
+/**
+ * @fileoverview a comment with a license tag.
+ *
+ * @license
+ * Some license
+ */
+goog.module('test_files.file_comment.puretransform.fileoverview_comment_add_suppress_before_license');
+var module = module || { id: 'test_files/file_comment.puretransform/fileoverview_comment_add_suppress_before_license.ts' };
+module = module;
+// here comes code.
+exports.x = 1;
