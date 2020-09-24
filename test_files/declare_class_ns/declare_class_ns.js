6341f774ccab4b9d50df498b57a0dbe7c3c3b123
Evan Martin <evanm@google.com>
2020-09-24 15:12:23 -0700

    move some tests to use a d.ts
    These tests only use 'declare' and aren't modules, so move them to a d.ts
    to make that intent more clear.
    
    PiperOrigin-RevId: 333608048

--- test_files/declare_class_ns/declare_class_ns.js
+++
@@ -1,8 +0,0 @@
-/**
- * @fileoverview added by tsickle
- * Generated from: test_files/declare_class_ns/declare_class_ns.ts
- * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
- */
-goog.module('test_files.declare_class_ns.declare_class_ns');
-var module = module || { id: 'test_files/declare_class_ns/declare_class_ns.ts' };
-goog.require('tslib');
--- test_files/declare_class_overloads/declare_class_overloads.js
+++
@@ -1,8 +0,0 @@
-/**
- * @fileoverview added by tsickle
- * Generated from: test_files/declare_class_overloads/declare_class_overloads.ts
- * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
- */
-goog.module('test_files.declare_class_overloads.declare_class_overloads');
-var module = module || { id: 'test_files/declare_class_overloads/declare_class_overloads.ts' };
-goog.require('tslib');
