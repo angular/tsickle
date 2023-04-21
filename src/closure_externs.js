e8b1760640cc13e244905e3a73cd0d444c9ff695
tsjs-language-eng <tsjs-language-eng@google.com>
2023-04-21 10:25:15 -0700

    Duplicate webCrypto.CryptoKey{Pair} to global namespace
    PiperOrigin-RevId: 526074635

--- src/closure_externs.js
+++ src/closure_externs.js
@@ -87,11 +87,6 @@ var WindowOrWorkerGlobalScope;
 /** @record */
 function CanvasDrawImage() {}
 
-/** @constructor */
-function CryptoKey() {};
-/** @constructor */
-function CryptoKeyPair() {};
-
 /** @typedef {!{handleEvent: function(Event):void}} */
 var EventListenerObject;
 
