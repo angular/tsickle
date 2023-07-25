7c141985360aa6914d8523a844321fc2c5f92ca0
Laura Harker <lharker@google.com>
2023-07-24 17:34:37 -0700

    Add test reproducing tsickle bug outputting module$contents$ type annotations
    PiperOrigin-RevId: 550721049

---
+++ test_files/clutz_unexported_type.no_externs/user.js
@@ -0,0 +1,15 @@
+/**
+ *
+ * @fileoverview
+ * Generated from: test_files/clutz_unexported_type.no_externs/user.ts
+ * @suppress {dangerousUnrecognizedTypeError}
+ * TODO(b/238438892): remove the need for this suppression
+ *
+ */
+goog.module('test_files.clutz_unexported_type.no_externs.user');
+var module = module || { id: 'test_files/clutz_unexported_type.no_externs/user.ts' };
+goog.require('tslib');
+const tsickle_goog_unexported_type_1 = goog.requireType("unexported_type");
+const goog_unexported_type_1 = goog.require('unexported_type');
+/** @type {!module$contents$unexported_type_LocalClass} */
+exports.x = (0, goog_unexported_type_1.fn)();
